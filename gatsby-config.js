aec5b2298f9b158af16e43587b9e72d6e3c7e3a8
Jonathan Wade <jonathan.wade@quantum-health.com>
2023-04-03 10:25:28 -0400

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,26 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+ require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `QH Marketing Wiki`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: [
+    {
+      resolve: 'gatsby-source-prismic',
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO,
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+      },
+    },
+    "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
